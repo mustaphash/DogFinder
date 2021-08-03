3b79fdb162cf5afeeb414d68b6a239481f27e177
Mustafa Shakir <mshakir9918@gmail.com>
2021-08-03 21:50:35 +0300

    Add find more dog photo from API

---
+++ src/External.Finder/Query/GetMultiByBreed.cs
@@ -0,0 +1,38 @@
+﻿using External.Finder;
+using External.Finders.Query.Interface;
+using Models;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace External.Finders.Query
+{
+    public class GetMultiByBreed : IGetMultiByBreed
+    {
+        private readonly Context _httpClientContext;
+
+        public GetMultiByBreed()
+            : this(new Context())
+        {
+        }
+
+        public GetMultiByBreed(Context httpClientContext)
+        {
+            _httpClientContext = httpClientContext;
+        }
+
+        public async Task<ByBreed> ExecuteAsync()
+        {
+            int numberOfImages = int.Parse(Console.ReadLine());
+            var httpClient = _httpClientContext.GetClient();
+            HttpResponseMessage response = await httpClient.GetAsync($"https://dog.ceo/api/breeds/image/random/{numberOfImages}");
+            string content = await response.Content.ReadAsStringAsync();
+
+            var breeds = JsonConvert.DeserializeObject<ByBreed>(content);
+
+            return breeds;
+        }
+
+    }
+}
---
+++ src/Models/ByBreed.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Models
+{
+    public class ByBreed
+    {
+        public List<string> Message { get; set; }
+        public string Status { get; set; }
+    }
+}
